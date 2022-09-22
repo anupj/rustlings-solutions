1a550a07aba211740b454f64230c6b19d69bb078
Anup Jadhav <anup.jadhav@gmail.com>
2022-09-22 15:55:37 +0100

    Victory! I Fe-nished it

--- exercises/conversions/try_from_into.rs
+++ exercises/conversions/try_from_into.rs
@@ -23,8 +23,6 @@ enum IntoColorError {
     IntConversion,
 }
 
-// I AM NOT DONE
-
 // Your task is to complete this implementation
 // and return an Ok result of inner type Color.
 // You need to create an implementation for a tuple of three integers,
@@ -38,6 +36,18 @@ enum IntoColorError {
 impl TryFrom<(i16, i16, i16)> for Color {
     type Error = IntoColorError;
     fn try_from(tuple: (i16, i16, i16)) -> Result<Self, Self::Error> {
+        if let (r, g, b) = tuple {
+            if (u8::try_from(r).is_err() || u8::try_from(g).is_err() || u8::try_from(b).is_err()) {
+                return Err(Self::Error::IntConversion);
+            } else {
+                return Ok(Color {
+                    red: r as u8,
+                    blue: b as u8,
+                    green: g as u8,
+                });
+            }
+        }
+        Err(Self::Error::BadLen)
     }
 }
 
@@ -45,6 +55,18 @@ impl TryFrom<(i16, i16, i16)> for Color {
 impl TryFrom<[i16; 3]> for Color {
     type Error = IntoColorError;
     fn try_from(arr: [i16; 3]) -> Result<Self, Self::Error> {
+        if let [r, g, b] = arr {
+            if (u8::try_from(r).is_err() || u8::try_from(g).is_err() || u8::try_from(b).is_err()) {
+                return Err(Self::Error::IntConversion);
+            } else {
+                return Ok(Color {
+                    red: r as u8,
+                    blue: b as u8,
+                    green: g as u8,
+                });
+            }
+        }
+        Err(Self::Error::BadLen)
     }
 }
 
@@ -52,6 +74,18 @@ impl TryFrom<[i16; 3]> for Color {
 impl TryFrom<&[i16]> for Color {
     type Error = IntoColorError;
     fn try_from(slice: &[i16]) -> Result<Self, Self::Error> {
+        if let &[r, g, b] = slice {
+            if (u8::try_from(r).is_err() || u8::try_from(g).is_err() || u8::try_from(b).is_err()) {
+                return Err(Self::Error::IntConversion);
+            } else {
+                return Ok(Color {
+                    red: r as u8,
+                    blue: b as u8,
+                    green: g as u8,
+                });
+            }
+        }
+        Err(Self::Error::BadLen)
     }
 }
 
